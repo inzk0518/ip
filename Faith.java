431098661da5aaabe08ef79cf0bb2ef3590bb1a5
ivanng <ivanngzk0518@gmail.com>
2025-09-02 16:22:09 +0800

    Increment Level-1

--- Faith.java
+++ Faith.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Faith {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm Faith\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        System.out.println("    ____________________________________________________________\n" +
+                "    Hello! I'm Faith\n" +
+                "    What can I do for you?\n" +
+                "    ____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String line = scanner.nextLine();
+            System.out.println("    ____________________________________________________________");
+            if(line.equals("bye")) {
+                break;
+            }
+            System.out.println("    " + line);
+            System.out.println("    ____________________________________________________________");
+        }
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
