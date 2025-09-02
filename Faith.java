95ece1781779b493d604312427bd23ebd0701dac
ivanng <ivanngzk0518@gmail.com>
2025-09-02 16:50:23 +0800

    Add, List

--- Faith.java
+++ Faith.java
@@ -7,15 +7,26 @@ public class Faith {
                 "    What can I do for you?\n" +
                 "    ____________________________________________________________");
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int taskCount = 0;
 
         while(true) {
             String line = scanner.nextLine();
             System.out.println("    ____________________________________________________________");
-            if(line.equals("bye")) {
+            if (line.equals("bye")) {
                 break;
             }
-            System.out.println("    " + line);
-            System.out.println("    ____________________________________________________________");
+            if (line.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i + 1) + ". " + list[i]);
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                list[taskCount] = line;
+                taskCount++;
+                System.out.println("    added: " + line);
+                System.out.println("    ____________________________________________________________");
+            }
         }
         System.out.println("    Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
