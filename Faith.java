be78de3ca21a361ab32c4ecede05029f30e02152
ivanng <ivanngzk0518@gmail.com>
2025-09-03 01:31:50 +0800

    Handle Errors

--- Faith.java
+++ Faith.java
@@ -2,15 +2,12 @@ import java.util.Scanner;
 
 public class Faith {
     public static void main(String[] args) {
-        System.out.println("    ____________________________________________________________\n" +
-                "     Hello! I'm Faith\n" +
-                "     What can I do for you?\n" +
-                "    ____________________________________________________________");
+        System.out.println("    ____________________________________________________________\n" + "     Hello! I'm Faith\n" + "     What can I do for you?\n" + "    ____________________________________________________________");
         Scanner scanner = new Scanner(System.in);
         Task[] taskList = new Task[100];
         int taskCount = 0;
 
-        while(true) {
+        while (true) {
             String line = scanner.nextLine().trim();
             System.out.println("    ____________________________________________________________");
             if (line.equals("bye")) {
@@ -25,48 +22,72 @@ public class Faith {
                 }
                 System.out.println("    ____________________________________________________________");
             } else {
-                String[] parts = line.split(" ", 2);
-                if (parts[0].equals("todo")) {
-                    //Todo
-                    Task task = new Todo(parts[1]);
-                    taskList[taskCount++] = task;
-                    System.out.println("     Got it. I've added this task:");
-                    System.out.print("       ");
-                    System.out.println(task.toString());
-                    System.out.println("     Now you have " + taskCount + " tasks in the list.");
-                } else if (parts[0].equals("deadline")) {
-                    //Deadline
-                    String[] deadlineParts = parts[1].split("/by", 2);
-                    Task task = new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim());
-                    taskList[taskCount++] = task;
-                    System.out.println("     Got it. I've added this task:");
-                    System.out.print("       ");
-                    System.out.println(task.toString());
-                    System.out.println("     Now you have " + taskCount + " tasks in the list.");
-                } else if (parts[0].equals("event")) {
-                    //Event
-                    String[] eventParts = parts[1].split("/from", 2);
-                    String[] timeParts = eventParts[1].split("/to", 2);
-                    Task task = new Event(eventParts[0].trim(), timeParts[0].trim(), timeParts[1].trim());
-                    taskList[taskCount++] = task;
-                    System.out.println("     Got it. I've added this task:");
-                    System.out.print("       ");
-                    System.out.println(task.toString());
-                    System.out.println("     Now you have " + taskCount + " tasks in the list.");
-                } else if (parts[0].equals("mark")) {
-                    Task targetTask = taskList[Integer.parseInt(parts[1]) - 1];
-                   targetTask.markDone();
-                    System.out.println("     Nice! I've marked this task as done:");
-                    System.out.println("       " + targetTask);
-                } else if (parts[0].equals("unmark")) {
-                    Task targetTask = taskList[Integer.parseInt(parts[1]) - 1];
-                    targetTask.unmarkDone();
-                    System.out.println("     OK, I've marked this task as not done yet:");
-                    System.out.println("       " + targetTask);
-                } else {
-                    taskList[taskCount] = new Task(line);
-                    taskCount++;
-                    System.out.println("    added: " + line);
+                try {
+                    String[] parts = line.split(" ", 2);
+                    if (parts[0].equals("todo")) {
+                        //Todo
+                        if (parts.length < 2) {
+                            throw new IllegalArgumentException("     OOPS!!! The description of a todo cannot be empty.");
+                        }
+                        Task task = new Todo(parts[1]);
+                        taskList[taskCount++] = task;
+                        System.out.println("     Got it. I've added this task:");
+                        System.out.print("       ");
+                        System.out.println(task.toString());
+                        System.out.println("     Now you have " + taskCount + " tasks in the list.");
+                    } else if (parts[0].equals("deadline")) {
+                        //Deadline
+                        if (parts.length < 2) {
+                            throw new IllegalArgumentException("     OOPS!!! The description of a deadline cannot be empty.\n" +
+                                    "Please do it like this : deadline <description> /by <date>");
+                        }
+                        String[] deadlineParts = parts[1].split("/by", 2);
+                        if (deadlineParts.length != 2) {
+                            throw new IllegalArgumentException("     OOPS!!! The deadline is missing.\n" +
+                                    "Please do it like this : deadline <description> /by <date>");
+                        }
+                        Task task = new Deadline(deadlineParts[0].trim(), deadlineParts[1].trim());
+                        taskList[taskCount++] = task;
+                        System.out.println("     Got it. I've added this task:");
+                        System.out.print("       ");
+                        System.out.println(task.toString());
+                        System.out.println("     Now you have " + taskCount + " tasks in the list.");
+                    } else if (parts[0].equals("event")) {
+                        //Event
+                        if (parts.length < 2) {
+                            throw new IllegalArgumentException("     OOPS!!! The description of an event cannot be empty.");
+                        }
+                        String[] eventParts = parts[1].split("/from", 2);
+                        if (eventParts.length != 2) {
+                            throw new IllegalArgumentException("     OOPS!!! Invalid input. \n" +
+                                    "Please do it like this : event <description> /from <time> to: <time>");
+                        }
+                        String[] timeParts = eventParts[1].split("/to", 2);
+                        if (timeParts.length != 2) {
+                            throw new IllegalArgumentException("     OOPS!!! Invalid input. \n" +
+                                    "Please do it like this : event <description> /from <time> to: <time>");
+                        }
+                        Task task = new Event(eventParts[0].trim(), timeParts[0].trim(), timeParts[1].trim());
+                        taskList[taskCount++] = task;
+                        System.out.println("     Got it. I've added this task:");
+                        System.out.print("       ");
+                        System.out.println(task.toString());
+                        System.out.println("     Now you have " + taskCount + " tasks in the list.");
+                    } else if (parts[0].equals("mark")) {
+                        Task targetTask = taskList[Integer.parseInt(parts[1]) - 1];
+                        targetTask.markDone();
+                        System.out.println("     Nice! I've marked this task as done:");
+                        System.out.println("       " + targetTask);
+                    } else if (parts[0].equals("unmark")) {
+                        Task targetTask = taskList[Integer.parseInt(parts[1]) - 1];
+                        targetTask.unmarkDone();
+                        System.out.println("     OK, I've marked this task as not done yet:");
+                        System.out.println("       " + targetTask);
+                    } else {
+                        throw new IllegalArgumentException("     OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
                 }
                 System.out.println("    ____________________________________________________________");
             }
